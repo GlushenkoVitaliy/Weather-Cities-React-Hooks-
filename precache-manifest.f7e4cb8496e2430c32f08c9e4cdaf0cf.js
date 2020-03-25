036eb9104ea30f6c82e51c9076e9881d5461827e
Glushenko.Vitaliy <vts.ghlushenko@gmail.com>
2020-03-25 18:37:25 +0200

    Updates

---
+++ precache-manifest.f7e4cb8496e2430c32f08c9e4cdaf0cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad283204f9450fa7a661259cb7cfd73",
+    "url": "/Weather-Cities-React-Hooks/index.html"
+  },
+  {
+    "revision": "b40a6295843f94f3c9ea",
+    "url": "/Weather-Cities-React-Hooks/static/css/main.b024c3f9.chunk.css"
+  },
+  {
+    "revision": "d7ef3aa07c08e6ca6c14",
+    "url": "/Weather-Cities-React-Hooks/static/js/2.4a8b9b40.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Weather-Cities-React-Hooks/static/js/2.4a8b9b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40a6295843f94f3c9ea",
+    "url": "/Weather-Cities-React-Hooks/static/js/main.0729f1ca.chunk.js"
+  },
+  {
+    "revision": "5c36976e40f40a6b8178",
+    "url": "/Weather-Cities-React-Hooks/static/js/runtime-main.cf0132e1.js"
+  }
+]);
